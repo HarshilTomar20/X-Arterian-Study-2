f9ce1efdabe5d78fb5f4300e13a1b3e4f97f8bc7
HarshilTomar20 <harshiltomar20@gmail.com>
2024-07-30 12:16:04 +0530

    learn: mongodb init

---
+++ Backend/mongoosenodebasic/modals/schema.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+
+const ArticleSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    content: {
+        type: String,
+    },
+    date: { 
+        type: Date, 
+        default: Date.now 
+    },
+});
+
+const ArticleModel = mongoose.model("Article", ArticleSchema);
+export default ArticleModel;
